078c2f571362bcec55c076693058f9bf896ed37a
Stephen Buttolph <sjb274@cornell.edu>
2017-06-03 16:44:27 -0400

    added constants to AutoTrader

--- AutoTrader/src/tasks/AutoTrader.java
+++ AutoTrader/src/tasks/AutoTrader.java
@@ -2,30 +2,26 @@ package tasks;
 
 import java.util.concurrent.TimeUnit;
 
+import constants.Constants;
+
 public class AutoTrader extends RepeatTask {
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+	}
 
 	@Override
 	public long getInitialDelay() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Constants.AUTO_TRADER.INITIAL_DELAY;
 	}
 
 	@Override
 	public TimeUnit getTimeUnit() {
-		// TODO Auto-generated method stub
-		return null;
+		return Constants.AUTO_TRADER.TIME_UNIT;
 	}
 
 	@Override
 	public long getRepeatDelay() {
-		// TODO Auto-generated method stub
-		return 0;
+		return Constants.AUTO_TRADER.REPEAT_DELAY;
 	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
