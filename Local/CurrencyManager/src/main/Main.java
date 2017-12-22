491b8b23d9b1b1f37d04f074cd35503406d3be9a
sjb274 <sjb274@cornell.edu>
2017-12-22 17:20:52 -0500

    added initial empty java files

--- Local/CurrencyManager/src/main/Main.java
+++ Local/CurrencyManager/src/main/Main.java
@@ -1,7 +1,15 @@
 package main;
 
+import java.math.BigDecimal;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		BigDecimal dnum1 = new BigDecimal("0.25");
+		BigDecimal dnum2 = new BigDecimal("0.3");
+		BigDecimal dnum3 = dnum1.subtract(dnum2);
+
+		System.out.println(dnum1);
+		System.out.println(dnum2);
+		System.out.println(dnum3);
 	}
 }
