12257005ca10c25d6645ed29c1557454ec279a42
sjb274 <sjb274@cornell.edu>
2017-12-25 15:35:24 -0500

    made iterable functions slightly more flexible with types

--- Local/CurrencyManager/src/utils/IterableUtils.java
+++ Local/CurrencyManager/src/utils/IterableUtils.java
@@ -13,38 +13,36 @@ public class IterableUtils {
 		return new ArrayIterable<>(arr);
 	}
 
-	public static <A, B> Iterable<B> map(Iterable<A> init, Function<A, B> f) {
-		return new MappedIterable<>(init, f);
+	public static <A, B> Iterable<B> map(Iterable<A> iter, Function<? super A, ? extends B> f) {
+		return new MappedIterable<>(iter, f);
 	}
 
-	public static <A, B> B fold(Iterable<A> iter, BiFunction<B, A, B> f, B acc) {
+	public static <A, B> B fold(Iterable<? extends A> iter, BiFunction<? super B, ? super A, ? extends B> f, B acc) {
 		for (A val : iter) {
 			acc = f.apply(acc, val);
 		}
 		return acc;
 	}
+	
+	
 
-	
-	
-	
-	
 	private static class FlattenedIterable<T> implements Iterable<T> {
-		private Iterable<? extends Iterable<T>> iter;
+		private Iterable<? extends Iterable<? extends T>> iter;
 
-		private FlattenedIterable(Iterable<? extends Iterable<T>> iter) {
+		private FlattenedIterable(Iterable<? extends Iterable<? extends T>> iter) {
 			this.iter = iter;
 		}
 
 		@Override
 		public Iterator<T> iterator() {
-			return new FlattenedIterator<T>(iter);
+			return new FlattenedIterator<>(iter);
 		}
 
 		private static class FlattenedIterator<S> implements Iterator<S> {
 			Iterator<? extends S> currentIter;
-			private Iterator<? extends Iterable<S>> iters;
+			private Iterator<? extends Iterable<? extends S>> iters;
 
-			private FlattenedIterator(Iterable<? extends Iterable<S>> iters) {
+			private FlattenedIterator(Iterable<? extends Iterable<? extends S>> iters) {
 				this.iters = iters.iterator();
 			}
 
@@ -101,9 +99,9 @@ public class IterableUtils {
 
 	private static class MappedIterable<A, B> implements Iterable<B> {
 		private Iterable<A> init;
-		private Function<A, B> f;
+		private Function<? super A, ? extends B> f;
 
-		private MappedIterable(Iterable<A> init, Function<A, B> f) {
+		private MappedIterable(Iterable<A> init, Function<? super A, ? extends B> f) {
 			this.init = init;
 			this.f = f;
 		}
@@ -115,9 +113,9 @@ public class IterableUtils {
 
 		private static class MappedIterator<X, Y> implements Iterator<Y> {
 			private Iterator<X> init;
-			private Function<X, Y> f;
+			private Function<? super X, ? extends Y> f;
 
-			private MappedIterator(Iterable<X> init, Function<X, Y> f) {
+			private MappedIterator(Iterable<X> init, Function<? super X, ? extends Y> f) {
 				this.init = init.iterator();
 				this.f = f;
 			}
