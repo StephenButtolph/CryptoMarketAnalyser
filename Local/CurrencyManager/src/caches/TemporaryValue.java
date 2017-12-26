d173551f0ccbdaeecfc49640ae6a359ba65150e7
sjb274 <sjb274@cornell.edu>
2017-12-26 16:29:33 -0500

    added working gui example, and temporary value class

---
+++ Local/CurrencyManager/src/caches/TemporaryValue.java
@@ -0,0 +1,34 @@
+package caches;
+
+import java.time.Instant;
+import java.time.temporal.TemporalAmount;
+import java.util.function.Supplier;
+
+public class TemporaryValue<V> {
+	private Instant prevRefresh;
+	private TemporalAmount refreshPeriod;
+	
+	private Supplier<V> supplier;
+	private V value;
+
+	public TemporaryValue(Supplier<V> supplier, TemporalAmount refreshPeriod) {
+		this.supplier = supplier;
+		setRefreshPeriod(refreshPeriod);
+	}
+	
+	public V getValue() {
+		if (shouldRefresh()) {
+			value = supplier.get();
+			prevRefresh = Instant.now();
+		}
+		return value;
+	}
+	
+	public void setRefreshPeriod(TemporalAmount refreshPeriod) {
+		this.refreshPeriod = refreshPeriod;
+	}
+	
+	private boolean shouldRefresh() {
+		return prevRefresh == null || prevRefresh.plus(refreshPeriod).isAfter(Instant.now());
+	}
+}
