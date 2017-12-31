0700791c4b956c2def1488462b3f4c90d2a625a5
sjb274 <sjb274@cornell.edu>
2017-12-30 21:49:42 -0500

    added comments in Holding

--- Local/CurrencyManager/src/accounts/Holding.java
+++ Local/CurrencyManager/src/accounts/Holding.java
@@ -3,19 +3,47 @@ package accounts;
 import arithmetic.Pfloat;
 import currencies.Currency;
 
+/**
+ * @author Stephen Buttolph
+ * 
+ *         Represents a currency holding. This will include, the currency to
+ *         represent the units of the holding, the amount to represent the
+ *         magnitude of the holding, and the location the holding is stored in.
+ */
 public class Holding {
 	private Currency currency;
 	private Pfloat amount;
 
+	// TODO include the location of storage.
+
+	/**
+	 * Create a new holding with the given magnitude [amount] and the unit of
+	 * currency [currency].
+	 * 
+	 * @param currency
+	 *            The unit of currency that this holding will have.
+	 * @param amount
+	 *            The magnitude this holding will have.
+	 */
 	public Holding(Currency currency, Pfloat amount) {
 		this.currency = currency;
 		this.amount = amount;
 	}
 
+	/**
+	 * Get the unit of currency that this holding has.
+	 * 
+	 * @return The unit of currency that this holding has.
+	 */
 	public Currency getCurrency() {
 		return this.currency;
 	}
 
+	/**
+	 * Get the magnitude of this holding.
+	 * 
+	 * @return The magnitude of this holding.
+	 */
 	public Pfloat getAmount() {
 		return this.amount;
 	}
