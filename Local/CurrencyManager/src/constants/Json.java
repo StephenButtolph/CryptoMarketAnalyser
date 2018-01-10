5d76906efc869f5cf99111b063adfe8d7a4dfea6
sjb274 <sjb274@cornell.edu>
2018-01-10 12:00:41 -0500

    added Json comments

--- Local/CurrencyManager/src/constants/Json.java
+++ Local/CurrencyManager/src/constants/Json.java
@@ -2,7 +2,15 @@ package constants;
 
 import com.google.gson.Gson;
 
+/**
+ * @author Stephen Buttolph
+ *
+ *         This is used to store default Json serializers and deserializers.
+ */
 public class Json {
+	/**
+	 * Standard Json serializer and deserializer with Google's Gson library.
+	 */
 	public static final Gson GSON;
 
 	static {
