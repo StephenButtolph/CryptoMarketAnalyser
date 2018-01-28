32bf2045f99d71127b861ac797088a7823e53fb8
sjb274 <sjb274@cornell.edu>
2018-01-28 03:08:22 -0500

    added standard currency table implementation, same as tracking table without the tracking abilites

---
+++ Platform/CurrencyManager/src/main/java/applications/components/tables/currencyTables/StandardCurrencyTable.java
@@ -0,0 +1,41 @@
+package applications.components.tables.currencyTables;
+
+import java.time.Duration;
+
+import arithmetic.Pfloat;
+import platforms.currencies.Currency;
+import platforms.tickers.coinMarketCap.CoinMarketCap;
+
+public class StandardCurrencyTable extends CurrencyTable<CurrencyData> {
+	private final CoinMarketCap coinMarketCap;
+
+	public StandardCurrencyTable(CoinMarketCap coinMarketCap) {
+		this(coinMarketCap, null);
+	}
+
+	public StandardCurrencyTable(CoinMarketCap coinMarketCap, Duration autoRefreshRate) {
+		super(autoRefreshRate);
+
+		this.coinMarketCap = coinMarketCap;
+	}
+
+	@Override
+	protected CurrencyData loadRow(Currency currency) {
+		Pfloat floatRank = coinMarketCap.getRank(currency);
+
+		int rank;
+		if (floatRank.isDefined()) {
+			rank = Integer.parseInt(floatRank.toString());
+		} else {
+			return null;
+		}
+
+		Pfloat price = coinMarketCap.getUsdPrice(currency);
+		Pfloat marketCap = coinMarketCap.getMarketCap(currency);
+		Pfloat volume = coinMarketCap.get24HVolume(currency);
+
+		CurrencyData newData = new CurrencyData(currency, rank, currency.toString(), price, marketCap, volume);
+		return newData;
+	}
+
+}
